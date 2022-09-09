2cc6fe727ef49cbe292eb35587d6bb141d1006b3
Lizzy-More <chiderasunday10@gmail.com>
2022-09-08 17:32:01 -0700

    error corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Return: Always 0 (Success)
+ *Description: prints and that piece..
+ *Return: 1 if successful
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\
+			n", 59);
 
 	return (1);
 }
