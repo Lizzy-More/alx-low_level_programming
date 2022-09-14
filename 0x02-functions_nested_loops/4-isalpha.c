b03f0a06ca5f353225d229e6d4fbf4181ffc2789
Lizzy-More <chiderasunday10@gmail.com>
2022-09-14 10:12:14 -0700

    pha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,20 +1,19 @@
 include "main.h"
 
 /**
- * _isalpha - checks whether or not a character is an alphabet
- * @c: ascii value
- * Return: 1 if it is lowercase or 0 otherwise
+ * _isalpha - Return 1 if c is a letter. lower or uppercase
+ * @c: The int to print
+ * Return: Always 0.
  */
-
 int _isalpha(int c)
 {
-	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
 	{
 		return (1);
 	}
 	else
 	{
-		Return(0);
+		return (0);
 	}
-}
 
+}
