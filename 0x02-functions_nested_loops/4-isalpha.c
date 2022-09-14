26f3fe478254c4e12e444393d318c07c1a64bf2c
Lizzy-More <chiderasunday10@gmail.com>
2022-09-14 10:33:09 -0700

    is

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,18 @@
 include "main.h"
-
 /**
- * _isalpha - Return 1 if c is a letter. lower or uppercase
- * @c: The int to print
- * Return: Always 0.
+ * _isalpha - main function
+ *
+ * @c: character to compare
+ *
+ * Return: 1 if true, 0 if false
+ *
  */
+
 int _isalpha(int c)
 {
-	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
-
+	return (0);
 }
