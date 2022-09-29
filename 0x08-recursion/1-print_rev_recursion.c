40820ca7cad7b0a30f1109b1dbaded778f6dcc32
Lizzy-More <chiderasunday10@gmail.com>
2022-09-29 14:11:05 -0700

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion -> printing string in reverse using recursion
+ * _print_rev_recursion - printing string in reverse using recursion
  * @s: string s
  */
 void _print_rev_recursion(char *s)
